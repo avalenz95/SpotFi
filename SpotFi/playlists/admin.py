79cf301fe6933c3f8a32d31aac464324047a47e2
ablades <audaris@gmail.com>
2019-12-12 15:47:16 -0800

    add song and playlist to admin site

--- SpotFi/playlists/admin.py
+++ SpotFi/playlists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from playlists.models import Song, Playlist
 
 # Register your models here.
+
+admin.site.register(Song)
+admin.site.register(Playlist)
--- SpotFi/playlists/models.py
+++ SpotFi/playlists/models.py
@@ -1,16 +1,22 @@
+
+from django.conf import settings
 from django.db import models
 
-
-
-class Playlists(model.Models):
+class Playlist(models.Model):
     title = models.CharField(max_length=30)
-    number_of_songs = model.PositiveIntegerField()
+    number_of_songs = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
 
 
-class Song(model.Models):
+class Song(models.Model):
     title = models.CharField(max_length=30)
-    runtime = models.DecimalField(decimal_places=2)
+    runtime = models.DecimalField(max_digits=3, decimal_places=2)
     artist = models.CharField(max_length=30)
-    playlists = models.ForeignKey(Playlists, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
