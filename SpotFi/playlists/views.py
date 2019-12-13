55cc16a07be668fb5abc539421c622ee2ee0f598
ablades <audaris@gmail.com>
2019-12-12 16:20:46 -0800

    add staticfiles

--- SpotFi/playlists/views.py
+++ SpotFi/playlists/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
+from playlists.models import Song, Playlist
 
-# Create your views here.
+
+class PlaylistListView(ListView):
+
+    def get(self, request):
+        """ GET a list of Pages. """
+        #pages = Page.objects.order_by('-created')
+        #pages = Page.objects.order_by('created'.desc())
+        pages = self.get_queryset().all().order_by('-created')
+        #pages = self.get_queryset().all()
+        return render(request, 'list.html', {
+          'pages': pages
+        })
+
