b44c480edd89d14f2ea25598062d73d5a347560b
ablades <audaris@gmail.com>
2019-12-12 17:38:38 -0800

    move urls around

--- SpotFi/SpotFi/urls.py
+++ SpotFi/SpotFi/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+
+    
     #Playlists app
-    path('', include('playlists.urls'))
+    path('playlists/', include('playlists.urls'))
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SpotFi/playlists/urls.py
+++ SpotFi/playlists/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PlaylistListView.as_view(), name='playlist-list-view'),
+    path('playlists/', views.PlaylistListView.as_view(), name='playlist-list-view'),
 ]
